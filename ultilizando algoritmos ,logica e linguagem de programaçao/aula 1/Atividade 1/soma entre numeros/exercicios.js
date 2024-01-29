108660d20bf0c1f50b710d29c64dc8e8ac7bf2dc
Rômulo Henrique Ferreira Gonçalves <rhfg2021@hotmail.com>
2024-01-29 19:35:16 -0300

    Ultilizando typescript para soma entre numeros

---
+++ ultilizando algoritmos ,logica e linguagem de programaçao/aula 1/Atividade 1/soma entre numeros/exercicios.js
@@ -0,0 +1,16 @@
+//alerta ("hello world")
+
+// var numero1;
+// var numero2;
+/*var numero1, numero2, soma;
+
+numero1 = 3;
+numero2 = 6;
+
+/soma = numero1 + numero2;
+
+/alerta (soma);*/
+
+var numero1 = prompt("Digite o primeiro número");
+
+alert(numero1 = numero1);
