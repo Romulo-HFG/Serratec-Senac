42f859d77a662cb90ad889ae27d67d5dc8a2736c
Rômulo Henrique Ferreira Gonçalves <rhfg2021@hotmail.com>
2024-01-29 21:25:54 -0300

    typescript para nome e data de nascimento

---
+++ ultilizando algoritmos ,logica e linguagem de programaçao/aula 2/Atividade 1/nome-nascimento/exercicios.js
@@ -0,0 +1,18 @@
+/*const numeroUm: number = 1
+
+console.log(numeroUm)
+
+let nome:string = ""
+
+nome = prompt ("digite seu nome")
+
+console.log(`Seja bem vindo ${nome}`)
+
+document.write(`Seja bem vindo ${nome}`)
+
+alert(`seja bem vindo ${nome}`)*/
+var nascimento = 0;
+nascimento = prompt("digite o ano do seu nascimento");
+var nascimento = 2030 - nascimento;
+console.log("voc\u00EA ter\u00E1 ".concat(nascimento, " anos"));
+alert("voc\u00EA ter\u00E1 ".concat(nascimento, " anos"));
---
+++ ultilizando algoritmos ,logica e linguagem de programaçao/aula 2/Atividade 1/nome-nascimento/exercicios.ts
@@ -0,0 +1,26 @@
+/*const numeroUm: number = 1 
+
+console.log(numeroUm)
+
+let nome:string = ""
+
+nome = prompt ("digite seu nome")
+
+console.log(`Seja bem vindo ${nome}`)
+
+document.write(`Seja bem vindo ${nome}`)
+
+alert(`seja bem vindo ${nome}`)*/
+
+
+
+
+let nascimento:number = 0    
+
+nascimento = prompt ("digite o ano do seu nascimento")
+
+let nascimento= 2030 - nascimento
+
+console.log(`você terá ${nascimento} anos`)
+
+alert(`você terá ${nascimento} anos`)
